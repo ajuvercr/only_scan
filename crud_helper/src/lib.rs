ff36f867897065c5404b01e4e64e48a33473c611
Arthur Vercruysse <arthur.vercruysse@persgroep.net>
2022-01-11 15:34:17 +0100

    implement TryInto and update

--- crud_helper/src/lib.rs
+++ crud_helper/src/lib.rs
@@ -1,13 +1,9 @@
 #![feature(proc_macro_internals)]
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::{quote, ToTokens};
+use quote::quote;
 use syn::{
-    parse::Error,
-    punctuated::Punctuated,
-    spanned::Spanned,
-    token::{Comma, Token},
-    DeriveInput, Field,
+    parse::Error, punctuated::Punctuated, spanned::Spanned, token::Comma, DeriveInput, Field,
 };
 
 type Result<T> = std::result::Result<T, Error>;
@@ -58,6 +54,96 @@ fn set_fields(ast: &mut DeriveInput, fields: Vec<Field>) -> Result<()> {
     Ok(())
 }
 
+fn non_field(f: Field) -> TokenStream2 {
+    let ident = &f.ident;
+    quote! {
+       #ident: None,
+    }
+}
+
+fn builder_f(inp: &DeriveInput, new_ident: &syn::Ident) -> Result<TokenStream2> {
+    let fields = get_fields(inp)?;
+    let non_fields: TokenStream2 = fields.iter().cloned().map(non_field).collect();
+
+    let check_fields: TokenStream2 = fields
+        .iter()
+        .cloned()
+        .map(|f| {
+            let f_ident = &f.ident.unwrap();
+            let name = f_ident.to_string();
+            quote! {
+              if self.#f_ident.is_none() {
+                errors.push(String::from(#name));
+              }
+            }
+        })
+        .collect();
+
+    let output: TokenStream2 = fields
+        .iter()
+        .cloned()
+        .map(|f| {
+            let f_ident = &f.ident;
+            quote! {
+              #f_ident: self.#f_ident.unwrap(),
+            }
+        })
+        .collect();
+
+    let update_f: TokenStream2 = fields
+        .iter()
+        .cloned()
+        .map(|f| {
+            let f_ident = &f.ident;
+            quote! {
+                if let Some(x) = self.#f_ident {
+                  this.#f_ident = x;
+                }
+            }
+        })
+        .collect();
+
+    let indent = &inp.ident;
+    let generics = &inp.generics;
+
+    Ok(quote! {
+      impl #generics #new_ident #generics {
+        pub fn new() -> Self {
+          Self {
+            #non_fields
+          }
+        }
+
+        pub fn update(self, this: &mut #indent #generics) {
+            #update_f
+
+        }
+      }
+
+      impl #generics TryInto<#indent #generics> for #new_ident #generics {
+        type Error = Vec<String>;
+        fn try_into(self) -> std::result::Result<#indent #generics, Vec<String>> {
+          let mut errors = Vec::new();
+          #check_fields
+
+          if(errors.is_empty()) {
+              Ok(#indent {
+                #output
+              })
+          } else {
+              Err(errors)
+          }
+        }
+      }
+
+      impl #generics #indent #generics {
+          pub fn builder() -> #new_ident {
+            #new_ident::new()
+          }
+      }
+    })
+}
+
 fn wrap_option(mut f: Field) -> Result<Field> {
     let ty = &f.ty;
     let quoted = quote! { ::std::option::Option<#ty> };
@@ -103,12 +189,13 @@ fn apply_crud(ast: DeriveInput) -> Result<TokenStream> {
 
     let mut new_ast = ast.clone();
     set_fields(&mut new_ast, option_fields)?;
-    new_ast.ident = syn::Ident::new(&format!("{}Patch", ast.ident), ast.ident.span());
 
-    let mut quoted = quote! {#new_ast};
+    new_ast.ident = syn::Ident::new(&format!("{}Builder", ast.ident), ast.ident.span());
+
+    let builder = builder_f(&ast, &new_ast.ident)?;
+
+    let mut quoted = quote! {#new_ast #builder};
     quoted.extend(fields.iter().map(|f| with_field(&new_ast, f)));
+
     Ok(quoted.into())
 }
-
-#[cfg(test)]
-mod tests {}
--- crud_helper/tests/testing.rs
+++ crud_helper/tests/testing.rs
@@ -1,21 +1,29 @@
 use crud_helper::*;
 
 pub trait Builder<T> {
-  fn with(self, t: T) -> Self;
+    fn with(self, t: T) -> Self;
 }
-
 
 #[derive(Crud, Clone)]
 struct Story {
-  #[id] id: String,
-  name: String,
+    #[id]
+    id: String,
+    name: String,
 }
-
 
 #[test]
 fn it_works() {
-    
-    let mut story = Story { id: "test".into(), name: "test".into() };
+    let mut story = Story {
+        id: "test".into(),
+        name: "test".into(),
+    };
+
+    let builder = Story::builder().with_name("test2".into());
+
+    assert_eq!(story.name, String::from("test"));
+    builder.update(&mut story);
+
+    assert_eq!(story.name, String::from("test2"));
 
     let result = 2 + 2;
     assert_eq!(result, 4);
